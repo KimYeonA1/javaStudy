3bad77299380ec07511d66343af0adb56b232dbd
KimYeonA1 <85yuna@naver.com>
2024-11-20 15:01:23 +0900

    오라클 데이타베이스 연동 및 callableStatement 테스팅 (in mode, out mode)

--- bookTest2/src/bookTest2/BookMenu.java
+++ bookTest2/src/bookTest2/BookMenu.java
@@ -1,12 +1,11 @@
 package bookTest2;
 
 public interface BookMenu {
-	//생성자가 안 들어감
-	//상수, 추상메소드, 디폴트, Private
+	//상수, 추상메소드, 디폴트, Priviate
 	int PRINT = 1;
 	int INSERT = 2;
 	int UPDATE = 3;
-	int DELETE = 4; 
-	int EXIT = 5;
-	
+	int DELETE = 4;
+	int SALARY_UP = 5; 
+	int EXIT = 6;
 }
--- bookTest2/src/bookTest2/BooksMain.java
+++ bookTest2/src/bookTest2/BooksMain.java
@@ -1,20 +1,20 @@
 package bookTest2;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 //import oracle.jdbc.driver.DBConversion;
 
 public class BooksMain {
 	public static Scanner scan = new Scanner(System.in);
-	
-	
+
 	public static void main(String[] args) throws SQLException {
 		boolean exitFlag = false;
 		// 사용자로부터 Books 출력, 입력, 수정, 삭제 , 종료요청받는다.
@@ -34,6 +34,9 @@ public class BooksMain {
 			case BookMenu.DELETE:
 				booksDelete();
 				break;
+			case BookMenu.SALARY_UP:
+				employeeSalaryUp();
+				break;
 			case BookMenu.EXIT:
 				exitFlag = true;
 				break;
@@ -44,6 +47,32 @@ public class BooksMain {
 		System.out.println("The end");
 	}
 
+	// 연봉인상 10%
+	private static void employeeSalaryUp() throws SQLException {
+		// Connection
+		Connection con = null;
+		CallableStatement cstmt = null; 
+		// 1 Load,2 connect
+		con = DBConnection.dbCon();
+		System.out.print("인상될 ID 입력: >>");
+		int id = Integer.parseInt(scan.nextLine()); 
+		System.out.print("인상금액: >>");
+		int price = Integer.parseInt(scan.nextLine()); 
+		// 3. cstmt = con.prepareCall("{call EMP1_PROCEDURE(?,?,?)}");
+		cstmt =con.prepareCall("{call BOOKS_PROCEDURE(?, ?, ?)}");
+		cstmt.setInt(1, id);
+		cstmt.setDouble(2, price);
+		// 출력될 데이터값으로 3번을 바인딩시킨다.
+		cstmt.registerOutParameter(3, Types.VARCHAR);
+		
+		int result = cstmt.executeUpdate();
+		String message = cstmt.getString(3);
+		System.out.println(message);
+		// 4.내용이 잘 입력이 되었는지 check
+		System.out.println((result != 0) ? "책값 인상 프로시저성공" : "책값 인상 프로시저실패");
+		// 6.sql 객체 반남
+		DBConnection.dbClose(con, cstmt);
+	}
 	// 삭제
 	private static void booksDelete() throws SQLException {
 		// Connection
@@ -55,10 +84,8 @@ public class BooksMain {
 		// 3.statement
 		System.out.print("삭제할번호>>");
 		int no = Integer.parseInt(scan.nextLine());
-		
-		pstmt = con.prepareStatement("DELETE FROM BOOKS WHERE ID = ?");
+		pstmt = con.prepareStatement("DELETE FROM BOOKS WHERE ID = ? ");
 		pstmt.setInt(1, no);
-		
 		int result = pstmt.executeUpdate();
 		// 4.내용이 잘 입력이 되었는지 check
 		System.out.println((result != 0) ? "삭제성공" : "삭제실패");
@@ -76,9 +103,9 @@ public class BooksMain {
 		// 1 Load,2 connect
 		con = DBConnection.dbCon();
 		// 3.statement
-		//수정할데이터를 입력
+		// 수정할데이터를 입력
 		Books books = new Books(3, "JAVA java", "kdj", "2024", 44000);
-		
+		// stmt = con.createStatement();
 		pstmt = con.prepareStatement("UPDATE BOOKS SET  TITLE = ?, PUBLISHER = ?, YEAR = ? , PRICE = ?  WHERE ID = ? ");
 		pstmt.setString(1, books.getTitle());
 		pstmt.setString(2, books.getPublisher());
@@ -86,36 +113,33 @@ public class BooksMain {
 		pstmt.setInt(4, books.getPrice());
 		pstmt.setInt(5, books.getId());
 		int result = pstmt.executeUpdate();
-		
 		// 4.내용이 잘 입력이 되었는지 check
 		System.out.println((result != 0) ? "수정성공" : "수정실패");
 		// 6.sql 객체 반남
 		DBConnection.dbClose(con, stmt);
-
 	}
 
 	// 삽입
 	private static void booksInsert() throws SQLException {
 		// Connection
 		Connection con = null;
-		Statement stmt = null;
 		PreparedStatement pstmt = null;
 
 		// 1 Load,2 connect
 		con = DBConnection.dbCon();
 		// 3.statement
 		Books books = new Books(0, "Head First JAVA", "kdj", "2008", 23000);
-		pstmt = con.prepareStatement("INSERT INTO books VALUES (BOOKS_ID_SEQ.nextval,? ,?,?,?)");
+		pstmt = con.prepareStatement("INSERT INTO books VALUES (BOOKS_ID_SEQ.nextval, ?, ?, ?, ?)");
 		pstmt.setString(1, books.getTitle());
 		pstmt.setString(2, books.getPublisher());
 		pstmt.setString(3, books.getYear());
 		pstmt.setInt(4, books.getPrice());
-		
-		int result = pstmt.executeUpdate();	
+
+		int result = pstmt.executeUpdate();
 		// 4.내용이 잘 입력이 되었는지 check
 		System.out.println((result != 0) ? "입력성공" : "입력실패");
 		// 6.sql 객체 반남
-		DBConnection.dbClose(con, stmt);
+		DBConnection.dbClose(con, pstmt);
 
 	}
 
@@ -148,8 +172,8 @@ public class BooksMain {
 		DBConnection.dbClose(con, stmt, rs);
 	}
 
-	private static void printMenu() {     //각 테이블마다 있는 게 좋음 ex)booksMenu
-		System.out.println("Books Menu(1.출력, 2.입력, 3.수정  4.삭제  5.종료)");
+	private static void printMenu() {
+		System.out.println("Books Menu(1.출력, 2.입력, 3.수정  4.삭제  5.책값인상 6.종료");
 		System.out.print(">>");
 	}
 
